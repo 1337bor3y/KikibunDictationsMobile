3f46cb2040666f4c642e8716fc4b84ce7dc21bba
1337bor3y <boreichuck.maxim@lll.kpi.ua>
2025-03-07 13:02:03 +0200

    Implement take photo button alignment depends on screen orientation

--- text-recognition/src/main/java/com/bor3y/textrecognition/CameraPreviewScreen.kt
+++ text-recognition/src/main/java/com/bor3y/textrecognition/CameraPreviewScreen.kt
@@ -1,15 +1,13 @@
 package com.bor3y.textrecognition
 
+import android.content.res.Configuration
 import androidx.camera.compose.CameraXViewfinder
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentSize
@@ -19,6 +17,7 @@ import androidx.compose.material.icons.filled.PhotoCamera
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -26,9 +25,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.LifecycleOwner
@@ -62,39 +63,49 @@ fun CameraPreviewContent(
 ) {
     val surfaceRequest by viewModel.surfaceRequest.collectAsStateWithLifecycle()
     val context = LocalContext.current
-    val windowPadding = WindowInsets.navigationBars.asPaddingValues()
 
     LaunchedEffect(lifecycleOwner) {
         viewModel.bindToCamera(context.applicationContext, lifecycleOwner)
     }
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        surfaceRequest?.let { request ->
-            CameraXViewfinder(
-                surfaceRequest = request,
-                modifier = modifier
-            )
-        }
+    Scaffold { padding ->
+        Box(modifier = Modifier.fillMaxSize()) {
+            surfaceRequest?.let { request ->
+                CameraXViewfinder(
+                    surfaceRequest = request,
+                    modifier = modifier
+                )
+            }
 
-        IconButton(
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .padding(bottom = windowPadding.calculateBottomPadding() + 10.dp)
-                .background(Color.White, shape = CircleShape),
-            onClick = {
-                viewModel.takePhoto { bitmap ->
-                    bitmap?.let {
-                        // TODO: Show image with frame
+            IconButton(
+                modifier = Modifier
+                    .align(if (isLandscape()) Alignment.CenterEnd else Alignment.BottomCenter)
+                    .padding(
+                        bottom = padding.calculateBottomPadding() + 10.dp,
+                        end = padding.calculateRightPadding(LayoutDirection.Ltr) + 10.dp
+                    )
+                    .background(Color.White, shape = CircleShape),
+                onClick = {
+                    viewModel.takePhoto { bitmap ->
+                        bitmap?.let {
+                            // TODO: Show image with frame
+                        }
                     }
                 }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.PhotoCamera,
+                    contentDescription = "Take photo"
+                )
             }
-        ) {
-            Icon(
-                imageVector = Icons.Default.PhotoCamera,
-                contentDescription = "Take photo"
-            )
         }
     }
+}
+
+@Composable
+fun isLandscape(): Boolean {
+    val configuration = LocalConfiguration.current
+    return configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
 }
 
 @Composable
