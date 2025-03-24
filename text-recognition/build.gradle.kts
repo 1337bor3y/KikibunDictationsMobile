d16b5ef93dbf7ff000b338d4a8f888b0f2231b83
1337bor3y <boreichuck.maxim@lll.kpi.ua>
2025-03-24 09:28:57 +0200

    Add Hilt dependencies in text-recognition module

--- text-recognition/build.gradle.kts
+++ text-recognition/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.hiltAndroid)
 }
 
 android {
@@ -68,6 +70,8 @@ dependencies {
     // Icons
     implementation(libs.androidx.material.icons.extended)
 
-    // ViewModel
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    // Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+    implementation(libs.hilt.navigation.compose)
 }
