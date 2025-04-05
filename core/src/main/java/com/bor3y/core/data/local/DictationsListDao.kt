26861297ecf13706637e0e076a9d5ac3704fc4a6
1337bor3y <boreichuck.maxim@lll.kpi.ua>
2025-04-05 07:39:15 +0300

    Change suspend to flow in room getDictations

--- core/src/main/java/com/bor3y/core/data/local/DictationsListDao.kt
+++ core/src/main/java/com/bor3y/core/data/local/DictationsListDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
 import com.bor3y.core.data.local.entity.DictationItemEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface DictationsListDao {
@@ -13,7 +14,7 @@ interface DictationsListDao {
     suspend fun upsertDictations(dictations: List<DictationItemEntity>)
 
     @Query("SELECT * FROM dictationitementity")
-    suspend fun getDictations(): List<DictationItemEntity>
+    fun getDictations(): Flow<List<DictationItemEntity>>
 
     @Delete
     suspend fun deleteDictations(dictations: List<DictationItemEntity>)
