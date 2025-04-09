39c978effe471dcb181972705608e7e93d179039
1337bor3y <boreichuck.maxim@lll.kpi.ua>
2025-04-09 14:20:28 +0300

    Implement compose DictationItem

---
+++ dictations-list/src/main/java/com/bor3y/dictations_list/presentation/component/DictationItem.kt
@@ -0,0 +1,52 @@
+package com.bor3y.dictations_list.presentation.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bor3y.dictations_list.R
+
+@Composable
+fun DictationItem(
+    modifier: Modifier = Modifier,
+    title: String = "New Ways to See",
+    isNew: Boolean = true
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 4.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = title,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.weight(1f)
+        )
+        if (isNew) {
+            Text(
+                text = "New",
+                color = Color.Black,
+                modifier = Modifier
+                    .background(
+                        color = colorResource(R.color.new_dictation),
+                        shape = RoundedCornerShape(50)
+                    )
+                    .padding(horizontal = 8.dp, vertical = 4.dp),
+                fontSize = 12.sp
+            )
+        }
+    }
+}
